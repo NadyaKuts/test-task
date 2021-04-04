399cf05154403d521f08c37b95741ea87ba50b9c
NadyaKuts <nadiakuz97@gmail.com>
2021-04-05 00:22:32 +0300

    Your awesome message

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
     margin-left: 37px;
     margin-right: 38px;
     margin-top: 10px;
-    width: 25px;
+    width: 26px;
     height: 27px;
     background-image: no-repeat;
     background-size: contain;
